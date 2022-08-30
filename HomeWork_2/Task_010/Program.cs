40b3cb58a2ab4752819715b9e56ffa2803e250f4
Alexandr <am_deus@mail.ru>
2022-08-30 19:37:31 +0300

    Added Homework 2

---
+++ HomeWork_2/Task_010/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5; 782 -> 8; 918 -> 1
+
+Console.Clear ();
+
+/*
+dotnet new console
+dotnet run 
+*/
+
+//1 вариант
+/*
+GetNumber();
+GetNumber();
+GetNumber();
+void GetNumber()
+{
+Console.Write("Введите Ваше трёхзначное число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int number = ((numberA%100)-(numberA%10))/10;
+
+if((numberA>99) & (numberA<1000)){
+    Console.WriteLine($"Вторая цифра числа = {number}");
+}
+else{
+    Console.WriteLine($"Число не трёхзначно!!! Введите другое число!");
+}
+}
+*/
+
+// 2 вариант
+
+Console.Write("Введите Ваше число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+void GetNumber(int number)
+{
+if((number>99) & (number<1000)){
+    int numberB = ((number%100)-(number%10))/10;
+    Console.WriteLine($"Вторая цифра числа = {numberB}");
+}
+else{
+    Console.WriteLine($"Число не трёхзначно!!! Введите другое число");
+}
+}
+GetNumber (numberA);
+
---
+++ HomeWork_2/Task_015/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели и проверяет, является ли этот день выходным
+// 6 -> да; 7 -> да; 1 -> нет
+
+
+
+Console.Clear ();
+
+/*
+dotnet new console
+dotnet run 
+*/
+
+//1 вариант
+
+GetNumber();
+GetNumber();
+GetNumber();
+GetNumber();
+GetNumber();
+GetNumber();
+GetNumber();
+GetNumber();
+void GetNumber()
+{
+Console.Write("Введите число дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number>=8){
+    Console.WriteLine($"Парень ты пьян, иди проспись!");
+}
+else{
+    if(number>5 & number<8){
+        Console.WriteLine($"Ура, ВЫХОДНОЙ день");
+        }
+        else{
+            Console.WriteLine($"Работай негр, до выходных еще далеко(");
+        }
+}
+}
